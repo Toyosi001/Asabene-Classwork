fc49e186317e311d4a4535504dda63a9a4274649
Toyosi001 <salautoyosi23@gmail.com>
2022-08-08 16:32:38 +0100

    classwork

---
+++ work.js
@@ -0,0 +1,43 @@
+//Question 1
+let quote= "There is no exercise better for the heart than reaching down and lifting people up";
+console.log(quote);
+
+//Question 2
+let secondQuote= "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+console.log(secondQuote)
+
+//Question 3
+let type='10';
+console.log(typeof "10");
+console.log(10);
+let num=10;
+
+//Question 4
+let float="9.8";
+let figure= Number(9.8);
+console.log(Math.ceil(float));
+
+//5
+console.log("python".includes("on") && "jargon".includes("on"));
+
+//6
+console.log("i hope this course is not full of jargon".includes("jargon"));
+
+//7
+let number = Math.floor(Math.random() * 101);
+console.log(number);
+//8
+let numb = Math.floor(Math.random() * 50) + 50;
+console.log(numb);
+
+//9
+let numbe = Math.floor(Math.random() * 256);
+console.log(numbe);
+
+//10
+//11
+
+//12
+const sentence =
+  "You cannot end a sentence with because because because is a conjunction";
+console.log(sentence.substr(31, 23));
